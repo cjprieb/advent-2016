68fa9634921527eec7d08fa9b4c58d7b19fcb73f
cjprieb <cjprieb@gmail.com>
2017-12-03 09:20:04 -0600

    2017 Day 2 solution. also added f# tutorial project for reference

---
+++ Advent2017/Day02_Spreadsheet.fs
@@ -0,0 +1,68 @@
+﻿namespace Advent2017
+
+module Day02_Spreadsheet =
+    let CalculateChecksum (input:string list) = 
+        let rows = [
+            for line in input ->
+                [for x in (line.Split(' ','\t')) -> x]
+                |> List.map(fun x -> x |> int)
+        ]
+        let findMinMax row =
+            let getMinMax (min,max) x = 
+                match x with
+                | a when x < min -> (a,max)
+                | b when x > max -> (min,b)
+                | _ -> (min,max)
+            match row with 
+            | x::xs -> Seq.fold getMinMax (x,x) row
+            | [] -> (0,0)
+
+        let difference (min,max) = max - min
+
+        let checksum sumSoFar row = 
+            let diff = findMinMax row |> difference
+            sumSoFar + diff
+
+        Seq.fold checksum 0 rows
+        
+    let CalculateChecksum2 (input:string list) = 
+        let rows = [
+            for line in input ->
+                [for x in (line.Split(' ','\t')) -> x]
+                |> List.map(fun x -> x |> int)
+        ]
+        let divisor (x,y) =
+            match (x,y) with
+            | (_,0)|(0,_) -> 0
+            | (_,_) when x>y && x%y = 0 -> x/y
+            | (_,_) when y>x && y%x = 0 -> y/x
+            | (_,_) -> 0
+        
+        let checkForDivisor (head:int) (tail:int list) = 
+            let divisorCheck divisorSoFar x = 
+                //printfn "    Comparing %i and %i" head x
+                match divisorSoFar with
+                | 0 -> divisor (head,x)
+                | _ -> divisorSoFar
+
+            List.fold divisorCheck 0 tail
+        
+        let rec checkForDivisorRec (head:int) (tail:int list) = 
+            //printfn "  Head %i" head
+            let a = checkForDivisor head tail
+            let b = 
+                match tail with
+                | [] -> 0
+                | x::xs -> checkForDivisorRec x xs
+            if a > 0 then a else b
+
+        let checksum sumSoFar (row:int list) = 
+            //printfn "new row %A" row
+            let rowValue = 
+                match row with 
+                | [] -> 0
+                | head::tail -> checkForDivisorRec head tail
+            sumSoFar + rowValue
+
+        Seq.fold checksum 0 rows
+
---
+++ Advent2017Tests/Day02_Spreadsheet.fs
@@ -0,0 +1,41 @@
+﻿namespace Advent2017.Tests.Day02_Spreadsheet
+
+open Advent2017
+open NUnit.Framework
+
+[<TestFixture>]
+type Day01_Tests() = 
+
+    let Test1 input expected =
+        let actual = Day02_Spreadsheet.CalculateChecksum input
+        printfn "The spreadsheet checksum result is %i. %i was expected." expected actual
+        Assert.AreEqual(expected, actual)
+
+    let Test2 input expected =
+        let actual = Day02_Spreadsheet.CalculateChecksum2 input
+        printfn "The spreadsheet checksum result is %i. %i was expected." expected actual
+        Assert.AreEqual(expected, actual)
+        
+    [<Test>]
+    member this.Test1_3_Rows() = 
+        Test1 ["5 1 9 5";"7 5 3";"2 4 6 8"] 18
+
+    [<Test>]
+    member this.Answer1() =
+        // TODO: move to common location for use by other tests.
+        let dir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments)
+        let path = System.IO.Path.Combine(dir, "Visual Studio 2017", "Projects", "Advent", "Advent2017", "Resources", "Day02.txt")
+        let input = System.IO.File.ReadAllLines(path)
+        Test1 [for line in input -> line.Trim()] 41919
+        
+    [<Test>]
+    member this.Test2_3_Rows() = 
+        Test2 ["5 9 2 8";"9 4 7 3";"3 8 6 5"] 9
+
+    [<Test>]
+    member this.Answer2() =
+        // TODO: move to common location for use by other tests.
+        let dir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments)
+        let path = System.IO.Path.Combine(dir, "Visual Studio 2017", "Projects", "Advent", "Advent2017", "Resources", "Day02.txt")
+        let input = System.IO.File.ReadAllLines(path)
+        Test2 [for line in input -> line.Trim()] 303
