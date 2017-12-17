191e3382842fdf73a41df4aca272a4464dfe1773
cjprieb <cjprieb@gmail.com>
2017-12-17 09:40:09 -0600

    Day 16 and 17 solutions for 2017

---
+++ Advent2017/Day16_Promenade.fsx
@@ -0,0 +1,93 @@
+﻿#load "TestResources.fs"
+
+open System.Text.RegularExpressions
+
+let Spin n dancers =
+    //printfn "Spinning %i - %A" n dancers
+    let skip = (Array.length dancers) - n
+    let (first,last) = dancers |> Array.splitAt skip
+    Array.append last first
+
+let Exchange i j dancers =
+    //printfn "Exchanging %i with %i - %A" i j dancers
+    dancers
+    |> Array.mapi (fun idx d -> 
+        match idx with
+        | x when x = i -> dancers.[j]
+        | x when x = j -> dancers.[i]
+        | _ -> d
+    )
+
+let Partner a b dancers =
+    //printfn "Partnering %A with %A - %A" a b dancers
+    dancers
+    |> Array.map (fun d -> 
+        match d with
+        | x when x = b -> a
+        | x when x = a -> b
+        | _ -> d
+    )
+
+let Split s = s |> Array.ofSeq
+
+let ParseMove move =
+    let matches = Regex.Match (move, "([sxp])(\w+)(?:/(\w+))?")
+    if matches.Success then
+        let m = matches.Groups.[1].Value
+        let a = matches.Groups.[2].Value
+        let b = matches.Groups.[3].Value
+        match m with 
+        | "s" -> 
+            Some (Spin (a |> int))
+        | "x" ->
+            Some (Exchange (a |> int) (b |> int))
+        | "p" ->
+            Some (Partner (a.Chars 0) (b.Chars 0))
+        | _ ->
+            printfn "%A doesn't match pattern" move
+            None
+    else
+        printfn "%A doesn't match pattern" move
+        None
+
+let Parse (input:string) =
+    input.Split(',')
+    |> Seq.ofArray
+    |> Seq.choose ParseMove
+
+let Dance dancers moves = 
+    Seq.fold (fun dancers move -> move dancers) dancers moves;
+
+let actualDancers = "abcdefghijklmnop" |> Split
+let moves = Parse (TestResources.ReadAllLines "Day16.txt").Head
+
+let testDancers = "abcde" |> Split
+
+// solve1 actualDancers;;//fgmobeaijhdpkcln
+//Real: 00:00:00.022, CPU: 00:00:00.031, GC gen0: 3, gen1: 1, gen2: 0
+//val it : string = "fgmobeaijhdpkcln"
+let solve1 startingDancers =
+    moves
+    |> Dance startingDancers
+    |> Array.map (fun d -> d.ToString())
+    |> Array.reduce (+)
+
+// solve2 actualDancers;; // lgmkacfjbopednhi
+// Real: 00:00:00.577, CPU: 00:00:00.578, GC gen0: 70, gen1: 0, gen2: 0
+// val it : string = "lgmkacfjbopednhi"
+let solve2 startingDancers =
+    let max = 1000000000
+    let repeatsAt = 24
+    let (_,all) =
+        seq {1..repeatsAt}
+        |> Seq.fold (fun (dancers,all) idx -> 
+            let dancers' = Dance dancers moves
+            let all' = Map.add (idx-1) dancers all
+            (dancers',all')
+        ) (startingDancers,Map.empty)
+    let index = (max % repeatsAt)
+    printfn "Getting dance at index %i" index
+    let dancers = all.[index]
+    dancers
+    |> Array.map (fun d -> d.ToString())
+    |> Array.reduce (+)
---
+++ Advent2017/Day17_Spinlock.fsx
@@ -0,0 +1,77 @@
+﻿let Step idx increment list = (idx + increment) % (List.length list) + 1
+
+let Update idx iteration list = 
+    let (first,second) = List.splitAt idx list
+    List.concat [first;[iteration];second]
+
+let Evolve increment (idx,list) iteration =
+    let nextStep = Step idx increment list
+    let list' = Update nextStep iteration list
+    if nextStep = 1 then printfn "%i: %i" nextStep iteration
+    (nextStep,list')
+
+let Step2 idx increment iteration = (idx + increment) % (iteration) + 1
+
+let Evolve2 increment (idx,one) value =
+    let nextStep = Step2 idx increment (value)
+    let one' = 
+        if nextStep = 1 then 
+            printfn "%i: %i" nextStep value
+            value
+        else
+            one
+    if (value % 1000000) = 0 then printfn "#%i: %i" nextStep value
+    (nextStep,one')
+
+let testCases = [
+    0,0,[0]
+    1,1,[0;1]
+    2,1,[0;2;1]
+    3,2,[0;2;3;1]
+    4,2,[0;2;4;3;1]
+    5,1,[0;5;2;4;3;1]
+    6,5,[0;5;2;4;3;6;1]
+]
+
+let testOne increment prev curr =
+    let (_,idx,input) = prev
+    let (iteration,insertAt,expected) = curr
+    let (actualIdx, actualList) = Evolve increment (idx,input) iteration
+    if actualList = expected && actualIdx = insertAt then
+        printfn "Success! %i was inserted at %i: %A" iteration actualIdx actualList
+    else
+        printfn "Failed! %i was inserted at %i: %A, but %i and %A were expected" iteration actualIdx actualList insertAt expected
+
+let test =
+    testCases
+    |> List.windowed 2
+    |> List.map (fun x -> testOne 3 x.Head (List.last x))    
+
+// solve1 3;; 638
+// solve1 301;; 1642
+//Real: 00:00:00.021, CPU: 00:00:00.031, GC gen0: 10, gen1: 1, gen2: 0
+//val it : int = 1642
+//1: 1
+//1: 2
+//1: 4
+//1: 10
+//1: 17
+//1: 131
+let solve1 increment =
+    let evolveIncrement = Evolve increment
+    let (idx,list)=
+        [1..2017]
+        |> List.fold evolveIncrement (0,[0])
+    printfn "%A" list
+    list.[idx+1]
+    
+// solve2 301;; 33601318
+//Real: 00:00:08.053, CPU: 00:00:08.031, GC gen0: 700, gen1: 127, gen2: 2
+//val it : int = 33601318
+let solve2 increment =
+    let max = 50000000
+    let evolveIncrement = Evolve2 increment
+    let (_,one) =
+        [1..max]
+        |> List.fold evolveIncrement (0,0)
+    one
