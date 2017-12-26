92b1f491da92b4fc750a878fa78cd94efd96cda8
cjprieb <cjprieb@gmail.com>
2017-12-25 19:40:51 -0600

    Day 23 solution for 2017 (in both f# and c#)

---
+++ Advent2017/Day23_Coprocessor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    public class Day23_2017
+    {
+        private const int a = 1;
+        private static int b = 108100;
+        private const int c = 125100;
+        private static int d = 0;
+        //private static int e = 0;
+        private static int f = 0;
+        //private static int g = 0;
+        private static int h = 0;
+        private static long total = 0;
+
+        public static void Run()
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                //Print("While (true)");
+                f = 1;
+                d = 2;
+                int tmp = (int)Math.Sqrt(b);
+                total++;
+                while (d <= tmp && f != 0)
+                {
+                    //if (total > 20)
+                    //{
+                    //    Print("Stopping!");
+                    //    repeat = false;
+                    //    break;
+                    //}
+                    int divisor = b / d;
+                    if (b % d == 0)
+                    {
+                        Console.WriteLine("d {0} divides into b {1}", d, b);
+                        f = 0;
+                    }
+                    d += 1;
+                }
+                //Print("If (f == 0)");
+                if (f == 0)
+                {
+                    h += 1;
+                    //Console.WriteLine("Increasing h: {0}", h);
+                }
+                if (b == c)
+                {
+                    repeat = false;
+                    Print("Done");
+                }
+                else
+                {
+                    b += 17;
+                    //Print("Repeating");
+                }
+            }
+        }
+
+        private static void Print(string msg)
+        {
+            Console.WriteLine("Registers - " + msg);
+            Console.WriteLine("  a: {0}", a);
+            Console.WriteLine("  b: {0}", b);
+            Console.WriteLine("  c: {0}", c);
+            Console.WriteLine("  d: {0}", d);
+            //Console.WriteLine("  e: {0}", e);
+            Console.WriteLine("  f: {0}", f);
+            //Console.WriteLine("  g: {0}", g);
+            Console.WriteLine("  h: {0}", h);
+        }
+    }
+}
---
+++ Advent2017/Day23_Coprocessor.fsx
@@ -0,0 +1,120 @@
+﻿type DebugProcessor = 
+    {
+        registers:Map<char,int>
+        mulCount:int
+        instruction:int
+        totalCount:int
+    }
+
+type Value = RegisterValue of char | IntValue of int
+
+let get x registers =
+    match x with
+    | RegisterValue ch ->
+        if Map.containsKey ch registers then registers.[ch] else 0
+    | IntValue i -> 
+        i
+
+let print registers =
+    let str = 
+        ['a'..'h'] 
+        |> List.map (fun ch -> sprintf "%c = %i" ch (get (RegisterValue ch) registers))
+        |> List.reduce (fun s1 s2 -> s1 + "; " + s2)
+    sprintf "{%s}" str
+
+let set x y (state:DebugProcessor) =
+    //printfn "%2i Setting %A to %A" state.instruction x y
+    let registers' = Map.add x (get y state.registers) state.registers
+    {
+        registers=registers'
+        mulCount=state.mulCount
+        instruction=state.instruction+1
+        totalCount=state.totalCount+1
+    }
+
+let sub x y (state:DebugProcessor) =
+    //printfn "%2i Subtracting %A from %A" state.instruction y x
+    let value = (get (RegisterValue x) state.registers) - (get y state.registers) 
+    let registers' = Map.add x value state.registers
+    {
+        registers=registers'
+        mulCount=state.mulCount
+        instruction=state.instruction+1
+        totalCount=state.totalCount+1
+    }
+
+let mul x y (state:DebugProcessor) =
+    //printfn "%2i Multiplying %A by %A" state.instruction x y
+    let value = (get (RegisterValue x) state.registers) * (get y state.registers) 
+    let registers' = Map.add x value state.registers
+    {
+        registers=registers'
+        mulCount=state.mulCount+1
+        instruction=state.instruction+1
+        totalCount=state.totalCount+1
+    }
+
+let jnz x y (state:DebugProcessor) =
+    let cmpValue = get x state.registers
+    let jmpValue = get y state.registers
+    //printfn "%2i Jumping %A (%i) (if %A (%i) <> 0)" state.instruction y jmpValue x cmpValue
+    let instruction' = if cmpValue <> 0 then state.instruction + jmpValue else state.instruction + 1
+    {
+        registers=state.registers
+        mulCount=state.mulCount
+        instruction=instruction'
+        totalCount=state.totalCount+1
+    }
+
+let run (instructions:(DebugProcessor -> DebugProcessor) []) = 
+    //let startingState = {registers=Map.empty;mulCount=0;instruction=0;totalCount=0}
+    let startingState = {registers=(Map.add 'a' 1 Map.empty);mulCount=0;instruction=0;totalCount=0}
+    let rec loop state =
+        let instruction = instructions.[state.instruction]
+        let state' = instruction state
+        //printfn "  %s" (print state.registers)
+        //printfn "Multiply Count: %i" state.mulCount
+        if (state.totalCount % 1000 = 0) then printfn "h = %i" (get (RegisterValue 'h') state.registers)
+        if state'.instruction < 0 || state'.instruction >= (Array.length instructions) || state.totalCount > 100000
+        then
+            state'
+        else
+            loop state'
+    let endingState = loop startingState
+    (get (RegisterValue 'h') endingState.registers)
+
+let instructions = [|
+    set 'b' (IntValue 81)
+    set 'c' (RegisterValue 'b')
+    jnz (RegisterValue 'a') (IntValue 2)
+    jnz (IntValue 1) (IntValue 5)
+    mul 'b' (IntValue 100)
+    sub 'b' (IntValue -100000)
+    set 'c' (RegisterValue 'b')
+    sub 'c' (IntValue -17000)
+    set 'f' (IntValue 1)
+    set 'd' (IntValue 2)
+    set 'e' (IntValue 2)
+    set 'g' (RegisterValue 'd')
+    mul 'g' (RegisterValue 'e')
+    sub 'g' (RegisterValue 'b')
+    jnz (RegisterValue 'g') (IntValue 2)
+    set 'f' (IntValue 0)
+    sub 'e' (IntValue -1)
+    set 'g' (RegisterValue 'e')
+    sub 'g' (RegisterValue 'b')
+    jnz (RegisterValue 'g') (IntValue -8)
+    sub 'd' (IntValue -1)
+    set 'g' (RegisterValue 'd')
+    sub 'g' (RegisterValue 'b')
+    jnz (RegisterValue 'g') (IntValue -13)
+    jnz (RegisterValue 'f') (IntValue 2)
+    sub 'h' (IntValue -1)
+    set 'g' (RegisterValue 'b')
+    sub 'g' (RegisterValue 'c')
+    jnz (RegisterValue 'g') (IntValue 2)
+    jnz (IntValue 1) (IntValue 3)
+    sub 'b' (IntValue -17)
+    jnz (IntValue 1) (IntValue -23)
+|]
+
